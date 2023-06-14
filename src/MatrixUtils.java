bb939a22ace3e0d4a6dcf31b28eb4362194c085f
Цикура Марія <117911005+mariiatsykura@users.noreply.github.com>
2023-06-15 01:42:52 +0200

    Add files via upload

---
+++ src/MatrixUtils.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class MatrixUtils {
+    public static int calculateSum(int[][] matrix) {
+        int sum = 0;
+        for (int[] row : matrix) {
+            for (int element : row) {
+                sum += element;
+            }
+        }
+        return sum;
+    }
+}
